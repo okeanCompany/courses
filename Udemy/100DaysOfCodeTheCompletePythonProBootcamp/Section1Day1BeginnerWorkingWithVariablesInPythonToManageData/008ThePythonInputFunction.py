9478a8ab762d4577e8bb5fe69ebba978df33032a
okeanCompany <94703889+okeanCompany@users.noreply.github.com>
2024-09-11 20:02:14 +0200

    Delete Udemy/100DaysOfCodeTheCompletePythonProBootcamp directory

--- Udemy/100DaysOfCodeTheCompletePythonProBootcamp/Section1Day1BeginnerWorkingWithVariablesInPythonToManageData/008ThePythonInputFunction.py
+++
@@ -1,8 +0,0 @@
-print("Hello" + " " + "Angela")
-
-print("What is you name?")
-
-# input("What is your name?")
-
-# This line of code will take an input using the input() function
-print("Hello " + input("What is your name?") + "!")
--- Udemy/100DaysOfCodeTheCompletePythonProBootcamp/Section2Day2BeginnerUnderstandingDataTypesAndHowToManipulateStrings/016CodingExercise04BMICalculator.py
+++
@@ -1,7 +0,0 @@
-height = 1.65
-weight = 84
-
-# Calculate the bmi using weight and height.
-bmi = weight / (height ** 2)
-
-print(bmi)
